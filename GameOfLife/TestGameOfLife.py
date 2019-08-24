bbd8e097e20befabfe8d91ca6d6764e5253cc78f
Jose Solano R <josesolano@outlook.com>
2019-08-23 22:57:07 -0400

    Añadiendo Comentario de la prueba, acorde a las necesidades del juego

--- GameOfLife/TestGameOfLife.py
+++ GameOfLife/TestGameOfLife.py
@@ -4,7 +4,8 @@ from GameOfLife import Cell, CellState
 cell_state = CellState()
 cell = Cell()
 
-class Test_CellState(unittest.TestCase):
+# La celula deberia poder iniciarse con un estado: Vivo o Muerto
+class Test_InitializeCellState(unittest.TestCase):
     def test_cell_should_initialize_alive(self):
         cell.state = cell_state.ALIVE
         self.assertEqual(cell.state, cell_state.ALIVE)
@@ -12,4 +13,3 @@ class Test_CellState(unittest.TestCase):
     def test_cell_should_initialize_dead(self):
         cell.state = cell_state.DEAD
         self.assertEqual(cell.state, cell_state.DEAD)
-
