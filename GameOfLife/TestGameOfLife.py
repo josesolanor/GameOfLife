8429f38c6fd95ce1c43f899ee5916329af419e4b
Jose Solano R <josesolano@outlook.com>
2019-08-23 22:47:18 -0400

    cambiamos la prueba a: deberia inicializarse vivo y asignamos el valor de ALIVE

--- GameOfLife/TestGameOfLife.py
+++ GameOfLife/TestGameOfLife.py
@@ -2,10 +2,10 @@ import unittest
 from GameOfLife import Cell, CellState
 
 cell_state = CellState()
+cell = Cell()
 
 class Test_CellState(unittest.TestCase):
     def test_should_initialize_with_cell_state(self):
-        cell = Cell(cell_state.ALIVE);
+        cell.state = cell_state.ALIVE
         self.assertEqual(cell.state, cell_state.ALIVE)
-
 
