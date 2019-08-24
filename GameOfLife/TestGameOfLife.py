b7ef576335faba7dfc3691bcd5577bdacad90624
Jose Solano R <josesolano@outlook.com>
2019-08-23 22:33:48 -0400

    Prueba 1: Falla no existe una Clase CellState

--- GameOfLife/TestGameOfLife.py
+++ GameOfLife/TestGameOfLife.py
@@ -1,5 +1,7 @@
 import unittest
-from GameOfLife import Cell
+from GameOfLife import Cell, CellState
+
+cell_state = CellState()
 
 class Test_CellState(unittest.TestCase):
     def test_should_initialize_with_cell_state(self):
