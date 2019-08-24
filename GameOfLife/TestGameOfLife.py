ca8a3129db6bf3800b80bf7b02352a469772f166
Jose Solano R <josesolano@outlook.com>
2019-08-23 22:36:05 -0400

    Prueba 1: Falla, Motivo: Cell no toma argumentos

--- GameOfLife/TestGameOfLife.py
+++ GameOfLife/TestGameOfLife.py
@@ -7,3 +7,5 @@ class Test_CellState(unittest.TestCase):
     def test_should_initialize_with_cell_state(self):
         cell = Cell(cell_state.ALIVE);
         self.assertEqual(cell.state, cell_state.ALIVE)
+
+
