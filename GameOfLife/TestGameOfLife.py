f7e8730221db62883d08f8215662dbd886dc1972
Jose Solano R <josesolano@outlook.com>
2019-08-23 22:49:23 -0400

    Prueba 2: Deberia inicializarse muerta, funciona

--- GameOfLife/TestGameOfLife.py
+++ GameOfLife/TestGameOfLife.py
@@ -5,7 +5,11 @@ cell_state = CellState()
 cell = Cell()
 
 class Test_CellState(unittest.TestCase):
-    def test_should_initialize_with_cell_state(self):
+    def test_should_initialize_cell_alive(self):
         cell.state = cell_state.ALIVE
         self.assertEqual(cell.state, cell_state.ALIVE)
+
+    def test_should_initialize_cell_dead(self):
+        cell.state = cell_state.DEAD
+        self.assertEqual(cell.state, cell_state.DEAD)
 
