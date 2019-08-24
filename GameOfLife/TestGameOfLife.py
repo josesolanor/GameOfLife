43d3ac43cdef78095bf2be84ad1e35d41691b1d1
Jose Solano R <josesolano@outlook.com>
2019-08-23 23:07:17 -0400

    Prueba 3: Falla, Motivo: no existe el metodo next_generation_state

--- GameOfLife/TestGameOfLife.py
+++ GameOfLife/TestGameOfLife.py
@@ -3,6 +3,7 @@ from GameOfLife import Cell, CellState
 
 cell_state = CellState()
 cell = Cell()
+neighbors = None
 
 # La celula deberia poder iniciarse con un estado: Vivo o Muerto
 class Test_InitializeCellState(unittest.TestCase):
@@ -13,3 +14,20 @@ class Test_InitializeCellState(unittest.TestCase):
     def test_cell_should_initialize_dead(self):
         cell.state = cell_state.DEAD
         self.assertEqual(cell.state, cell_state.DEAD)
+
+# Reglas del juego a seguir:
+
+# Cualquier célula viva con menos de dos vecinos vivos muere.
+class Test_CellShouldDieLessTwoNeighbors(unittest.TestCase):
+    def test_cell_should_die_with_zero_neighbors(self):
+        neighbors = 0
+        cell.state = cell_state.ALIVE
+        cell.next_generation_state(neighbors)
+        self.assertEqual(cell.state, cell_state.DEAD)
+
+
+
+
+# Cualquier célula viva con dos o tres vecinos vivos sigue viviendo para la siguiente generación.
+# Cualquier célula viva con más de tres vecinos vivos muere.
+# Cualquier célula muerta con exactamente tres vecinos vivos se convierte en una célula viva.
